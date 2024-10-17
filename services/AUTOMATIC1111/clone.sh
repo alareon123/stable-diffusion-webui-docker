9774107c3ba9f15c38a348396bbb9b7dcd5b4181
Григорий <report@bnnm.ru>
2024-10-17 16:00:33 +0300

    init

--- services/AUTOMATIC1111/clone.sh
+++ services/AUTOMATIC1111/clone.sh
@@ -4,9 +4,13 @@ set -Eeuox pipefail
 
 mkdir -p /repositories/"$1"
 cd /repositories/"$1"
+export GIT_TRACE_PACKET=1
+export GIT_TRACE=1
+export GIT_CURL_VERBOSE=1
+git config --global core.compression 0
 git init
 git remote add origin "$2"
-git fetch origin "$3" --depth=1 || (sleep 5 && git fetch origin "$3" --depth=1)
+git fetch origin "$3" --depth=1 --unshallow || (sleep 5 && git fetch origin "$3" --depth=1)
 # Проверяем статус после выполнения команды
 if [ $? -ne 0 ]; then
     echo "Ошибка при выполнении git fetch"
